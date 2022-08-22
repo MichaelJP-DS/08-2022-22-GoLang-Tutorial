61f457a3a6a0e2e6255a52beefece8bfda6b8146
MichaelJP-DS <MichaelJP-DS@users.noreply.github.com>
2022-08-22 18:35:37 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,13 @@
+// Programs start running in package "main"
+package main
+
+// This program uses the packages with import paths "fmt" and "math/rand"
+import (
+    "fmt"
+)
+
+// Creating a function main()
+func main() {
+    fmt.Println("Michael Petrus")
+    fmt.Println("City: Geneva, State: IL")
+}
